2472e3a7ee553ed3923b7d49e3f6dcbd33bfba86
spi9325 <sanketinamdar72@gmail.com>
2025-08-12 00:36:02 +0530

    working code

---
+++ apps/backend/index.ts
@@ -0,0 +1,25 @@
+import { prismaClient } from "db/client";
+import express from "express"
+
+const app = express();
+app.use(express.json());
+
+app.get("/",async (req,res)=>{
+    console.log("hiiii backend");
+    const users =await prismaClient.user.findFirst({})
+    res.send(users);
+    console.log(users);
+})
+app.post("/add",async (req,res)=>{
+    console.log("hiiii backend");
+    const users =await prismaClient.user.create({
+        data:{
+            username:"sanket",
+            password:"123456"
+        }
+    })
+    res.send(users)
+    console.log(users);
+})
+
+app.listen(8080);
